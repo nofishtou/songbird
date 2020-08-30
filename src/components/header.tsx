09b6dd0c1ab742e0fda9df538df10851e045a286
nofishtou <damnedbyhimself@gmail.com>
2020-08-30 13:09:15 +0300

    fix: score bug and rerender bug

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React from "react";
 
 interface IHeaderProps {
-  score: number,
-  currentPosition: number,
+  score: number;
+  currentPosition: number;
 }
 
 export function Header(props: IHeaderProps): JSX.Element {
-  const positionNames = ['Разминка', 'Воробьиные', 'Лесные птицы', 'Певчие птицы', 'Хищные птицы', 'Морские птицы'];
+  const positionNames = [
+    "Разминка",
+    "Воробьиные",
+    "Лесные птицы",
+    "Певчие птицы",
+    "Хищные птицы",
+    "Морские птицы",
+  ];
 
   const renderPositions = (): JSX.Element[] => {
-    return (positionNames.map((name, index) => {
-      return (<li key={index} className={index === props.currentPosition ? "header-active" : ""}>{name}</li>)
-    }))
-  }
+    return positionNames.map((name, index) => {
+      return (
+        <li
+          key={index}
+          className={index === props.currentPosition ? "header-active" : ""}
+        >
+          {name}
+        </li>
+      );
+    });
+  };
 
   return (
     <header>
       <div className="header-upper-header">
-        <h1>Song<span className="color-red">Birds</span></h1>
+        <h1>
+          Song<span className="color-red">Birds</span>
+        </h1>
         <div>
           <span>Your Score:</span>
           <span className="header-score-number">{props.score}</span>
         </div>
       </div>
       <nav className="header-nav">
-        <ul>
-          {renderPositions()}
-        </ul>
+        <ul>{renderPositions()}</ul>
       </nav>
     </header>
-  )
+  );
 }
